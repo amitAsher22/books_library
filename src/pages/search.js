fa22210247a4429ed39228f16b2004e3bf671d9a
amita <amitasher4@gmail.com>
2023-03-03 17:37:38 +0200

    create search input

--- src/pages/search.js
+++ src/pages/search.js
@@ -6,52 +6,64 @@ import { useState } from "react";
 const ShowBooks = () => {
   const [valueInput, setValueInput] = useState("");
   const [listbooksData, setlisrbooksData] = useState(dataFromJson);
+  const [valueOfInput, setvalueOfInput] = useState("");
 
-  function handleChange(e) {
-    const valueFromInput = e.target.value;
-    setValueInput(valueFromInput);
+  // function handleChange(e) {
+  //   const valueFromInput = e.target.value;
+  //   setValueInput(valueFromInput);
 
-    if (valueInput === "") {
-      setlisrbooksData(listbooksData);
-    } else {
-      let filterBooks = listbooksData.filter(
-        (bookFilter) =>
-          bookFilter.title.toLowerCase().includes(valueInput.toLowerCase()) ||
-          bookFilter.writer.toLowerCase().includes(valueInput.toLowerCase())
-      );
+  //   if (valueInput === "") {
+  //     setlisrbooksData(listbooksData);
+  //   } else {
+  //     let filterBooks = listbooksData.filter(
+  //       (bookFilter) =>
+  //         bookFilter.title.toLowerCase().includes(valueInput.toLowerCase()) ||
+  //         bookFilter.writer.toLowerCase().includes(valueInput.toLowerCase())
+  //     );
 
-      setlisrbooksData(filterBooks);
-    }
-  }
+  //     setlisrbooksData(filterBooks);
+  //   }
+  // }
 
   return (
     <>
-      <div className={styles.divSearch}></div>
+      <div className={styles.divSearch}>
+        <input
+          placeholder="Search"
+          onChange={(e) => setvalueOfInput(e.target.value)}
+          className={styles.inputValue}
+        />
+      </div>
 
       <div className={styles.container}>
-        {listbooksData.map((book) => {
-          return (
-            <div key={book.id} className={styles.card}>
-              <div className={styles.containerImgBook}>
-                <img
-                  src={book.img}
-                  alt={book.title}
-                  className={styles.imgBook}
-                />
+        {valueInput === ""}
+        {listbooksData
+          .filter((item) => item.title.toLowerCase().includes(valueOfInput))
+          .map((book) => {
+            return (
+              <div key={book.id} className={styles.card}>
+                <div className={styles.containerImgBook}>
+                  <img
+                    src={book.img}
+                    alt={book.title}
+                    className={styles.imgBook}
+                  />
+                </div>
+                <h1>{book.title}</h1>
+                <div className={styles.show}>
+                  <Link
+                    href={`/book/${book.title
+                      .replace(/\s/g, "-")
+                      .toLowerCase()}`}
+                  >
+                    <button className={styles.button_link_book}>
+                      To The Book
+                    </button>
+                  </Link>
+                </div>
               </div>
-              <h1>{book.title}</h1>
-              <div className={styles.show}>
-                <Link
-                  href={`/book/${book.title.replace(/\s/g, "-").toLowerCase()}`}
-                >
-                  <button className={styles.button_link_book}>
-                    To The Book
-                  </button>
-                </Link>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </>
   );
