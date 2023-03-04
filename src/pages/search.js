976a9d75359927c9066f61eec6aae4a390fa356a
amita <amitasher4@gmail.com>
2023-03-04 12:38:44 +0200

    create new filter search input

--- src/pages/search.js
+++ src/pages/search.js
@@ -4,70 +4,64 @@ import dataFromJson from ".././data_books/books.json";
 import { useState } from "react";
 
 const ShowBooks = () => {
-  const [valueInput, setValueInput] = useState("");
   const [listbooksData, setlisrbooksData] = useState(dataFromJson);
   const [valueOfInput, setvalueOfInput] = useState("");
 
-  // function handleChange(e) {
-  //   const valueFromInput = e.target.value;
-  //   setValueInput(valueFromInput);
+  function handleChange(e) {
+    const valueFromInput = e.target.value;
 
-  //   if (valueInput === "") {
-  //     setlisrbooksData(listbooksData);
-  //   } else {
-  //     let filterBooks = listbooksData.filter(
-  //       (bookFilter) =>
-  //         bookFilter.title.toLowerCase().includes(valueInput.toLowerCase()) ||
-  //         bookFilter.writer.toLowerCase().includes(valueInput.toLowerCase())
-  //     );
+    setvalueOfInput(valueFromInput);
 
-  //     setlisrbooksData(filterBooks);
-  //   }
-  // }
+    if (valueOfInput === "") {
+      setlisrbooksData(listbooksData);
+    } else {
+      let filterBooks = dataFromJson.filter(
+        (bookFilter) =>
+          bookFilter.title
+            .toLowerCase()
+            .includes(valueFromInput.toLowerCase()) ||
+          bookFilter.writer.toLowerCase().includes(valueFromInput.toLowerCase())
+      );
+
+      setlisrbooksData(filterBooks);
+    }
+  }
 
   return (
     <>
       <div className={styles.divSearch}>
         <input
           placeholder="Search"
-          onChange={(e) => setvalueOfInput(e.target.value)}
+          onChange={handleChange}
           className={styles.inputValue}
+          value={valueOfInput}
         />
       </div>
 
       <div className={styles.container}>
-        {valueInput === ""}
-        {listbooksData
-          .filter(
-            (item) =>
-              item.title.toLowerCase().includes(valueOfInput) ||
-              item.writer.toLowerCase().includes(valueOfInput)
-          )
-          .map((book) => {
-            return (
-              <div key={book.id} className={styles.card}>
-                <div className={styles.containerImgBook}>
-                  <img
-                    src={book.img}
-                    alt={book.title}
-                    className={styles.imgBook}
-                  />
-                </div>
-                <h1>{book.title}</h1>
-                <div className={styles.show}>
-                  <Link
-                    href={`/book/${book.title
-                      .replace(/\s/g, "-")
-                      .toLowerCase()}`}
-                  >
-                    <button className={styles.button_link_book}>
-                      To The Book
-                    </button>
-                  </Link>
-                </div>
+        {listbooksData.map((book) => {
+          return (
+            <div key={book.id} className={styles.card}>
+              <div className={styles.containerImgBook}>
+                <img
+                  src={book.img}
+                  alt={book.title}
+                  className={styles.imgBook}
+                />
               </div>
-            );
-          })}
+              <h1>{book.title}</h1>
+              <div className={styles.show}>
+                <Link
+                  href={`/book/${book.title.replace(/\s/g, "-").toLowerCase()}`}
+                >
+                  <button className={styles.button_link_book}>
+                    To The Book
+                  </button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
